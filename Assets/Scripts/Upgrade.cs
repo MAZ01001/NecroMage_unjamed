59cf5b8473a98ad07eca14ffb4623b9a7c515526
MAZ <29178226+MAZ01001@users.noreply.github.com>
2023-01-06 00:07:20 +0100

    Update Upgrade.cs
    add options for multiple minions, player hp/xp, and minion unlocks
    also changed all attributes to pascal case

--- Assets/Scripts/Upgrade.cs
+++ Assets/Scripts/Upgrade.cs
@@ -1,28 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "Create Upgrade", fileName = "New Upgrade")]
 public class Upgrade : ScriptableObject{
     //~ inspector (public)
+    [Header("UI")]
+    [SerializeField][Tooltip("An image for displaying.")]                                                             public Image Icon;
+    [SerializeField][Min(0)][Tooltip("The level of the player required for this upgrade.")]                           public uint LevelRequirement = 1;
+    [SerializeField][Tooltip("The name of the upgrade.")]                                                             public string UpgradeName;
+    [SerializeField][Multiline][Tooltip("A short description for the upgrade.")]                                      public string UpgradeDescription;
     [Header("Upgrade information")]
-    [SerializeField][Tooltip("The minion (prefab) for wich the upgrade is for.")]                                     public GameObject minionPrefab;
-    [SerializeField][Tooltip("The name of the upgrade.")]                                                             public string upgradeName;
-    [SerializeField][Multiline][Tooltip("A short description for the upgrade.")]                                      public string upgradeDescription;
-    [SerializeField][Tooltip("An image for displaying.")]                                                             public Image icon;
+    [SerializeField][Range(0, 10)][Tooltip("Add to the players health points.")]                                      public int PlayerHP = 0;
+    [SerializeField][Min(0)][Tooltip("Add to the players experience points.")]                                        public int PlayerXP = 0;
+    [SerializeField][Tooltip("If this upgrade is a minion unlock of the following minions.")]                         public bool IsMinionUnlock = false;
+    [SerializeField][Tooltip("The minions (prefabs) for which the upgrade is for.")]                                  public List<GameObject> MinionPrefabs;
     [Header("Attack")]
-    [SerializeField][Min(0)][Tooltip("Add to damage per attack.")]                                                    public int damage = 0;
-    [SerializeField][Min(0f)][Tooltip("Add to attack delay in seconds.")]                                             public float timeBetweenAttacks = 0f;
+    [SerializeField][Min(0)][Tooltip("Add to damage per attack.")]                                                    public int Damage = 0;
+    [SerializeField][Min(0f)][Tooltip("Add to attack delay in seconds.")]                                             public float TimeBetweenAttacks = 0f;
     [Header("AOE (if available)")]
-    [SerializeField][Tooltip("Set the AOE active state.")]                                                            public bool areaOfEffectActive = false;
-    [SerializeField][Min(0f)][Tooltip("Add to AOE attack range.")]                                                    public float areaOfEffectRange = 0f;
-    [SerializeField][Min(0)][Tooltip("Add to AOE attack damage.")]                                                    public int areaOfEffectDamage = 0;
+    [SerializeField][Tooltip("Set the AOE active state.")]                                                            public bool AreaOfEffectActive = false;
+    [SerializeField][Min(0f)][Tooltip("Add to AOE attack range.")]                                                    public float AreaOfEffectRange = 0f;
+    [SerializeField][Min(0)][Tooltip("Add to AOE attack damage.")]                                                    public int AreaOfEffectDamage = 0;
     [Header("Projectile (if available)")]
-    [SerializeField][Tooltip("Set the projectile to spawn for range attacks, if set does no meele attacks anymore.")] public GameObject projectile = null;
-    [SerializeField][Tooltip("An offset for the projectile spawn.")]                                                  public Vector3 projectileOffset = Vector3.zero;
-    [SerializeField][Min(0f)][Tooltip("Add to the lifespan of the projectile in seconds.")]                           public float projectileMaxTimeAlive = 0f;
-    [SerializeField][Min(0f)][Tooltip("Add to the projectile speed in units per seconds.")]                           public float projectileSpeed = 0f;
+    [SerializeField][Tooltip("Set the projectile to spawn for range attacks, if set does no meele attacks anymore.")] public GameObject Projectile = null;
+    [SerializeField][Tooltip("An offset for the projectile spawn.")]                                                  public Vector3 ProjectileOffset = Vector3.zero;
+    [SerializeField][Min(0f)][Tooltip("Add to the lifespan of the projectile in seconds.")]                           public float ProjectileMaxTimeAlive = 0f;
+    [SerializeField][Min(0f)][Tooltip("Add to the projectile speed in units per seconds.")]                           public float ProjectileSpeed = 0f;
     [Header("Ranges")]
-    [SerializeField][Min(0f)][Tooltip("Add to the walking speed.")]                                                   public float walkingSpeed = 0f;
-    [SerializeField][Min(0f)][Tooltip("Add to the attack range.")]                                                    public float attackRange = 0f;
-    [SerializeField][Min(0f)][Tooltip("Add to the following range.")]                                                 public float followRange = 0f;
+    [SerializeField][Min(0f)][Tooltip("Add to the walking speed.")]                                                   public float WalkingSpeed = 0f;
+    [SerializeField][Min(0f)][Tooltip("Add to the attack range.")]                                                    public float AttackRange = 0f;
+    [SerializeField][Min(0f)][Tooltip("Add to the following range.")]                                                 public float FollowRange = 0f;
 }
