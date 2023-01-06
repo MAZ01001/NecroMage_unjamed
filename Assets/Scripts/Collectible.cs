e796d0c73b7d4f17f6c4c0c00f836785fea64aef
MAZ <29178226+MAZ01001@users.noreply.github.com>
2023-01-06 01:14:05 +0100

    Update Collectible.cs
    collectibles hold the upgrade and are checking for player contact

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
 
-class Collectible : MonoBehaviour{
-    //~ public
-    [HideInInspector] public float giveHP = 0f;
-    [HideInInspector] public float giveXP = 0f;
+public class Collectible : MonoBehaviour{
+    //~ inspector (private)
+    [SerializeField][Tooltip("the game manager object")] private GameManager gameManager;
+    [SerializeField][Tooltip("The upgrade to collect")]  private Upgrade upgrade;
+    //~ public (getter)
+    public Upgrade Upgrade => this.upgrade;
+
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject == this.gameManager.Player){
+            this.gameManager.ShowCollectibleScreen(this);
+        }
+    }
 }
